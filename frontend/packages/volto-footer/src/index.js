28da424026b5690ecfe695899e327945db631f71
Asier Iglesias <aiglesias@codesyntax.com>
2025-06-23 09:08:31 +0200

    functional volto-footer-project v2

--- frontend/packages/volto-footer/src/index.js
+++ frontend/packages/volto-footer/src/index.js
@@ -1,9 +1,14 @@
+import footerInherit from './reducers';
+
 const applyConfig = (config) => {
-  config.settings.isMultilingual = false;
-  config.settings.supportedLanguages = ['en'];
-  config.settings.defaultLanguage = 'en';
+  // Add footer inherit reducer
+  config.addonReducers = {
+    ...config.addonReducers,
+    footerInherit,
+  };
 
   return config;
 };
 
 export default applyConfig;
+export { getFooterInherit } from './actions';
